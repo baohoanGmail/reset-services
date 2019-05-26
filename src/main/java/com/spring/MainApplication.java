501d9d608659113bfefb533c46998e0461faf9fa
Anh <ntatvr@gmail.com>
2019-05-26 16:39:33 +0700

    Add logback

--- src/main/java/com/spring/MainApplication.java
+++ src/main/java/com/spring/MainApplication.java
@@ -1,20 +1,42 @@
 package com.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.spring")
+@RestController
 public class MainApplication extends SpringBootServletInitializer {
 
+  private static final Logger logger = LoggerFactory.getLogger(MainApplication.class);
+
   public static void main(String[] args) {
+    logger.info("this is a info message");
+    logger.warn("this is a warn message");
+    logger.error("this is a error message");
     SpringApplication.run(MainApplication.class, args);
   }
 
+  /**
+   * The @Value annotation is used to read the environment or application property value in Java
+   * code
+   */
+  @Value("${spring.application.name}")
+  private String name;
+
+  @RequestMapping(value = "/")
+  public String name() {
+    return name;
+  }
 }
--- src/main/java/com/spring/controllers/v1/ProductController.java
+++ src/main/java/com/spring/controllers/v1/ProductController.java
@@ -9,20 +9,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.spring.controllers.response.CommonResponse;
 import com.spring.entities.Product;
 import io.swagger.annotations.ApiOperation;
 
 @Controller
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1/products")
 @CrossOrigin(maxAge = 3600)
 public class ProductController {
 
   @ApiOperation(value = "Get all products", response = CommonResponse.class)
-  @GetMapping
+  @RequestMapping(method = RequestMethod.GET)
   @ResponseBody
   public ResponseEntity<Object> getAllAccounts(HttpServletRequest request,
       HttpServletResponse response) throws IOException {
